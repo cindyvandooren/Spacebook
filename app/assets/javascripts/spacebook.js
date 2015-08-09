d10128bb57672a1b149b8acd4d02cf65656b8735
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-08-09 14:20:29 -0700

    Killed turbolinks

---
+++ app/assets/javascripts/spacebook.js
@@ -0,0 +1,13 @@
+window.Spacebook = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Spacebook.initialize();
+});
