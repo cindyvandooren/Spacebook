ecdc55900fd595f5b76ee5210efd4ba698a04aaa
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-08-10 20:25:16 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root to: "static_pages#root"
+  root to: "static_pages#landing"
+  get "static_pages/landing" => "static_pages#landing", as: "landing"
+
+  resources :users, only: [:new, :create]
+  resource :sessions, only: [:new, :create, :destroy]
+  resources :static_pages, only: [:landing]
 end
