f77f2af99c95ed2bee5b7a79adb5b1284de0d7aa
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-08-12 07:47:09 -0700

    Added header profile picture and info subviews

--- app/assets/javascripts/views/profile/header.js
+++ app/assets/javascripts/views/profile/header.js
@@ -5,6 +5,8 @@ Spacebook.Views.ProfileHeader = Backbone.CompositeView.extend({
     var user = this.model;
     this.listenTo(user, "sync", this.render);
     this.addHeaderBackgroundView(user);
+    this.addHeaderInfoView(user);
+    this.addHeaderPictureView(user);
   },
 
   render: function () {
@@ -17,5 +19,15 @@ Spacebook.Views.ProfileHeader = Backbone.CompositeView.extend({
   addHeaderBackgroundView: function (user) {
     var subview = new Spacebook.Views.HeaderBackground({ model: user });
     this.addSubview(".profile-header-background", subview);
+  },
+
+  addHeaderInfoView: function (user) {
+    var subview = new Spacebook.Views.HeaderInfo({ model: user });
+    this.addSubview(".profile-header-info", subview);
+  },
+
+  addHeaderPictureView: function (user) {
+    var subview = new Spacebook.Views.HeaderPicture({ model: user });
+    this.addSubview(".profile-header-picture", subview);
   }
 });
