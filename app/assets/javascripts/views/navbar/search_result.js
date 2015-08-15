e96c6e0b9be620e455789605e39308cf88d129c0
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-08-15 11:15:09 -0700

    Added general modal for Search results

--- app/assets/javascripts/views/navbar/search_result.js
+++ app/assets/javascripts/views/navbar/search_result.js
@@ -1,4 +1,4 @@
-Spacebook.Views.SearchResult = Backbone.View.extend({
+Spacebook.Views.SearchResult = Backbone.CompositeView.extend({
   template: JST["navbar/search_result"],
 
   className: "search-result",
@@ -8,14 +8,10 @@ Spacebook.Views.SearchResult = Backbone.View.extend({
     this.listenTo(this.collection, "sync", this.render);
   },
 
-  // This is necessary to remove the modal view when the user wants to
-  // Will also help to prevent multiple Search Results existing at the
-  // same time. Look at Twitter example.
-  // events: {
-  //   "click modal-background" : "remove",
-  //   "click close-button" : "closeModalView",
-  //   "keyup" : "handleKey"
-  // },
+  events: {
+    "click .m-background" : "remove",
+    "click .close-button" : "closeModalView",
+  },
 
   render: function () {
     var renderedContent = this.template({ users: this.collection });
@@ -23,11 +19,11 @@ Spacebook.Views.SearchResult = Backbone.View.extend({
     return this;
   },
 
-  // closeModalView: function (event) {
-  //   event.preventDefault();
-  //   this.remove();
-  // },
-  //
+  closeModalView: function (event) {
+    event.preventDefault();
+    this.remove();
+  },
+
   handleKey: function (event) {
     if (event.keyCode === 27) {
       this.remove();
